3389cd33a08891377860e4a11ac02b8096449b88
Jimenez Ramirez Julio Fabian <julio.jimenez17@tectijuana.edu.mx>
2018-09-27 20:48:35 -0700

    Unidad2.Tarea1

---
+++ T2.1R.-JimenezRamirezJulioFabian/T2.1R.-JimenezRamirezJulioFabian/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace T2._1R._JimenezRamirezJulioFabian
+{
+   
+    public class Factorial
+    {
+ 
+        public int Operacion(int cantidad)
+        {
+          if(cantidad==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return cantidad * Operacion(cantidad - 1);
+
+            }
+            
+        }
+        ~Factorial()
+        {        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch Tiempo = Stopwatch.StartNew();        
+            int cantidad;
+            Console.Write("Escriba el numero del cual desea sacar el factorial ");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+            Factorial factorial = new Factorial();
+            Console.WriteLine("El resultado del factorial es {0} ",factorial.Operacion(cantidad));
+            Console.WriteLine("El tiempo que duró es:\t" + Tiempo.Elapsed);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tarea2.1.-JimenezRamirezJulioFabian/Tarea2.1.-JimenezRamirezJulioFabian/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Tarea2._1._JimenezRamirezJulioFabian
+{
+    public class Factorial
+    {
+     
+        public int Operacion()
+        {
+            int valor=1, cantidad;
+            Console.Write("Escriba el numero del cual desea sacar factorial ");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+            for (int contador=1;contador<=cantidad;contador++)
+            {
+                valor = valor * contador;
+            }
+            return valor;
+        }
+        ~Factorial()
+        { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch Tiempo = Stopwatch.StartNew();
+            Factorial fac = new Factorial();
+            Console.WriteLine("El resultado del factorial es {0} ",fac.Operacion());
+            Console.WriteLine("El tiempo que duró es:\t" + Tiempo.Elapsed);
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
