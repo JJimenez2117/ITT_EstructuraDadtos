206f7c444f4c99b789d8c61b4325318184be4376
Jimenez Ramirez Julio Fabian <julio.jimenez17@tectijuana.edu.mx>
2018-10-01 19:55:56 -0700

    P2.2 JimenezRamirezJulioFabian
    Fibonacci

---
+++ E2.2.-JimenezRamirezJulioFabian/E2.2.-JimenezRamirezJulioFabian/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace E2._2_JimenezRamirezJulioFabian
+{
+    public class Fibonacci
+    {
+
+        public int OperacionR(int cantidad)
+        {
+            if (cantidad <= 0)
+            {
+                return 1;
+            }
+
+            else
+            {
+                
+                return OperacionR(cantidad - 2) + OperacionR(cantidad - 1);
+               
+
+
+
+            }
+
+        }
+        public void Iteracion()
+        {
+            Stopwatch Tiempo = Stopwatch.StartNew();
+            int inicial = 0, final = 1, suma = 0, cantidad;
+            Console.Write("Escriba el numero hasta el cual desea realizar el fibonacci ");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+            for (int contador = 1; contador <= cantidad; contador++)
+            {
+                inicial = suma;
+                suma = final;
+                final = inicial + final;
+                Console.Write(suma + ",");
+            }
+            Console.WriteLine("El tiempo que duró la iteracion es:\t" + Tiempo.Elapsed);
+
+        }
+
+        ~Fibonacci()
+        { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int cantidad;
+            Console.Write("Escriba el numero hasta el cual desea realizar el fibonacci ");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+            Fibonacci fibonacci = new Fibonacci();
+            fibonacci.OperacionR(cantidad);
+            fibonacci.Iteracion();
+            Console.ReadKey();
+        }
+    }
+
+}
