c5ffa22bd3a93c2efe3096cbd8063ff4492ae7e2
Jimenez Ramirez Julio Fabian <julio.jimenez17@tectijuana.edu.mx>
2018-10-08 17:27:45 -0700

    EXAMEN U2
    examen de la suma de un numero de 7 digitos o más

---
+++ ExamenU2.-JimenezRamirezJulioFabian/ExamenU2.-JimenezRamirezJulioFabian/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenU2._JimenezRamirezJulioFabian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion = "";
+            string cadena;
+            do
+            {
+                try
+                {
+                    Console.Write("Ingrese un número de 7 dígitos o más: ");
+                    cadena = Console.ReadLine();
+                    if (cadena.Length < 7)
+                        throw new Exception("Por favor ingrese mas de 7 dígitos");
+
+                    Recursividad llamar = new Recursividad();
+
+                    Console.WriteLine("El resultado es: {0}", llamar.Sumar(cadena, cadena.Length, 0));
+
+                    Console.Write("¿Desea seguir usando el programa?\nPresione 1 para continuar, cualquier otra tecla para salir: ");
+                    opcion = Console.ReadLine();
+                    Console.Clear();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadKey();
+                    Console.Clear();
+                    opcion = "1";
+                }
+            } while (opcion == "1");
+
+        }
+    }
+}
+
---
+++ ExamenU2.-JimenezRamirezJulioFabian/ExamenU2.-JimenezRamirezJulioFabian/Recursividad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenU2._JimenezRamirezJulioFabian
+{
+    class Recursividad
+    {
+        public int Sumar(string cadena, int veces, int suma)
+        {
+            char[] valores;
+            valores = cadena.ToCharArray();
+            if (veces > 0)
+                suma = Convert.ToInt32(Convert.ToString(valores[veces - 1])) + Sumar(cadena, veces - 1, suma);
+            return suma;
+        }
+    }
+
+}
