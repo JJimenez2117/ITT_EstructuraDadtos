a41b69dfe6a53d1b917ee8756b7c29bf4c4b7682
Jimenez Ramirez Julio Fabian <julio.jimenez17@tectijuana.edu.mx>
2018-10-10 20:39:12 -0700

    EC.-JimenezRamirezJulioFabian

---
+++ EC.-JimenezRamirezJulioFabian/EC.-JimenezRamirezJulioFabian/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace EC._JimenezRamirezJulioFabian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack ejemplo = new Stack();
+
+            ejemplo.Push(1);
+            ejemplo.Push(82);
+            ejemplo.Push(5);
+
+            if (ejemplo.Contains(5))//Permite saber si existe el elmento dentro de la pila
+                Console.WriteLine("5");
+            else
+                Console.WriteLine("No contiene 5");
+
+            ejemplo.ToString();//Convierte un elemento en string
+
+            if (ejemplo.GetType() == typeof(Stack))//Se puede saber si un objeto es del tipo que desea
+                Console.WriteLine("Es el tipo que quiere");
+            else
+                Console.WriteLine("No es el tipo");
+
+            Console.WriteLine(ejemplo.ToArray().ElementAt(1) + "\n");//Convierte la pila en arreglo
+
+            IEnumerator enumerator = ejemplo.GetEnumerator();//Se consigue el enumerador de la pila
+            while (enumerator.MoveNext())
+            {
+                Object objeto = enumerator.Current;
+                Console.WriteLine(objeto);
+            }
+            Console.ReadKey();
+        }
+    }
+}
