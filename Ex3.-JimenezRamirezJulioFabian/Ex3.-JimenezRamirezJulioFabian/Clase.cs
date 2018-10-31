7f3b9ec77c5e3d21da127687e17b7ff21d9fb34d
JJimenez2117 <43395765+JJimenez2117@users.noreply.github.com>
2018-10-31 16:14:39 -0700

    Examen unidad 3
    examen unidad 3

---
+++ Ex3.-JimenezRamirezJulioFabian/Ex3.-JimenezRamirezJulioFabian/Clase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3._JimenezRamirezJulioFabian
+{
+    public class Clase
+    {
+        public string Maestro { get; set; }
+        public string Alumno { get; set; }
+        public string NombreClase { get; set; }
+        public int Calificacion { get; set; }
+
+        public Clase()
+        {
+
+        }
+
+    }
+}
