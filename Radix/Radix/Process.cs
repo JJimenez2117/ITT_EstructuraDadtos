bdd110395677116d0365e333dea3bb3fba2eb82f
JJimenez2117 <43395765+JJimenez2117@users.noreply.github.com>
2018-12-02 22:40:10 -0800

    Radix
    Deje los comentarios a medias

---
+++ Radix/Radix/Process.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radix
+{
+    public class Process
+    {
+        public void RadixSort(LinkedList<int> linkedList)
+        {
+            bool Terminado = false;//Se crea un bool que dice que terminado es falso
+            int posicion = 0;//posicion del digito
+
+            var digitos = new List<Queue<int>>();//Se crea una cola 
+            Agregar(digitos);
+
+            while (!Terminado)
+            {
+                Terminado = true;
+
+                foreach (int valor in linkedList)
+                {
+                    int elemento = ObtenerD(valor, posicion);
+                    if (elemento > 0)
+                    {
+                        Terminado = false;
+                    }
+
+                    digitos[elemento].Enqueue(valor);
+                }
+
+                var avanzar = linkedList.First;
+                foreach (var item in digitos)
+                {
+                    while (item.Count > 0 && avanzar != null)
+                    {
+                        avanzar.Value = item.Dequeue();
+                        avanzar = avanzar.Next;
+                    }
+                }
+
+                posicion++;
+            }
+
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static int ObtenerD(int valor, int posicion)//Se obtiene el digito
+        {
+            int Dig = (valor / (int)Math.Pow(10, posicion)) % 10;
+            return Dig;
+        }
+
+        static void Agregar(List<Queue<int>> elemento)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var cola = new Queue<int>();//
+                elemento.Add(cola);
+            }
+        }
+    }
+}
---
+++ Radix/Radix/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process radix = new Process();
+            LinkedList<int> owo = new LinkedList<int>();
+            Console.WriteLine("Escriba la cantidad de elementos");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < cantidad; i++)
+            {
+                int read = 0;
+                Console.Write("Escriba el elemento {0}: ", i + 1);
+                read = Convert.ToInt32(Console.ReadLine());
+                owo.AddLast(read);
+            }
+           
+
+
+            radix.RadixSort(owo);
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
